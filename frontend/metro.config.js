84a3d9f80c69f2244138b7eea3a856bc4d639e97
dominiklangbein <ge94nic@mytum.de>
2025-04-26 15:28:45 +0200

    sharriiing!!!!

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,7 +1,11 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
+// Learn more https://docs.expo.dev/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const { withShareExtension } = require('expo-share-extension/metro');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-module.exports = config;
+// Hier wird die Share Extension eingebunden
+module.exports = withShareExtension(config, {
+  isCSSEnabled: true, // optional, für Web-Support (kannst auch false oder weglassen)
+});
