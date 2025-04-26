4ff786521f47ed209bd8ec7284878f2779348eef
Moritz Glowatzki <moritz.glowatzki@gmail.com>
2025-04-26 16:18:36 +0200

    new try

--- frontend/app/(tabs)/explore.tsx
+++ frontend/app/(tabs)/explore.tsx
@@ -5,18 +5,18 @@ import { ExternalLink } from '@/components/ExternalLink';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { IconSymbol } from '@/components/ui/IconSymbol';
 import React from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function TabTwoScreen() {
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
       headerImage={
-        <IconSymbol
+        <MaterialIcons
           size={310}
           color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
+          name="chevron-left"
           style={styles.headerImage}
         />
       }>
